8e02d4196f3c298b124817560774b0f1b44b1add
tutucloud1 <ihgkurd@gmail.com>
2025-05-18 15:34:27 +0800

    Update setup_hysteria.sh

--- setup_hysteria.sh
+++ setup_hysteria.sh
@@ -1,22 +1,19 @@
 mkdir -p /opt/hysteria-server
 mkdir -p /opt/hysteria-server/certs/live/tutusub.xyz
 
-
 curl -o /opt/hysteria-server/docker-compose.yml https://o.oo0o.ooo/scripts/installment/tutu/docker-compose.yml 
 curl -o /opt/hysteria-server/cloudflare.ini https://o.oo0o.ooo/scripts/installment/tutu/cloudflare.ini 
 curl -o /opt/hysteria-server/server.yaml https://o.oo0o.ooo/scripts/installment/tutu/server.yaml 
 
-
 curl -L https://raw.githubusercontent.com/tutucloud1/dfhasjkdhasjkhda-dsadsa/main/fullchain.pem -o /opt/hysteria-server/certs/live/tutusub.xyz/fullchain.pem
 curl -L https://raw.githubusercontent.com/tutucloud1/dfhasjkdhasjkhda-dsadsa/main/privkey.pem -o /opt/hysteria-server/certs/live/tutusub.xyz/privkey.pem
-
 
 chmod 644 /opt/hysteria-server/certs/live/tutusub.xyz/fullchain.pem
 chmod 644 /opt/hysteria-server/certs/live/tutusub.xyz/privkey.pem
 
-mkdir -p /var/lib/docker/volumes/opt_hysteria-server_certbot_etc/_data/live/tutusub.xyz/
-ln -sf /opt/hysteria-server/certs/live/tutusub.xyz/fullchain.pem /var/lib/docker/volumes/opt_hysteria-server_certbot_etc/_data/live/tutusub.xyz/fullchain.pem
-ln -sf /opt/hysteria-server/certs/live/tutusub.xyz/privkey.pem /var/lib/docker/volumes/opt_hysteria-server_certbot_etc/_data/live/tutusub.xyz/privkey.pem
+sed -i 's|/etc/letsencrypt/live/tutucloud.shop/|/etc/letsencrypt/live/tutusub.xyz/|g' /opt/hysteria-server/server.yaml
+
+sed -i '/hysteria:/,/server.yaml/ s|volumes:|volumes:\n      - ./certs/live/tutusub.xyz:/etc/letsencrypt/live/tutusub.xyz|' /opt/hysteria-server/docker-compose.yml
 
 docker compose -f /opt/hysteria-server/docker-compose.yml up -d hysteria
 
