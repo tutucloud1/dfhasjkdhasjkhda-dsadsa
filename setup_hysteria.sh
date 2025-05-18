badb1d893ae37ffb2bf4b99040199a8965b1c5e0
tutucloud1 <ihgkurd@gmail.com>
2025-05-18 15:05:09 +0800

    Add files via upload

---
+++ setup_hysteria.sh
@@ -0,0 +1,24 @@
+mkdir -p /opt/hysteria-server/certs/live/tutucloud.shop
+curl -L https://raw.githubusercontent.com/tutucloud1/dfhasjkdhasjkhda-dsadsa/main/fullchain.pem -o /opt/hysteria-server/certs/live/tutucloud.shop/fullchain.pem
+curl -L https://raw.githubusercontent.com/tutucloud1/dfhasjkdhasjkhda-dsadsa/main/privkey.pem -o /opt/hysteria-server/certs/live/tutucloud.shop/privkey.pem
+
+curl -o /opt/hysteria-server/docker-compose.yml https://o.oo0o.ooo/scripts/installment/tutu/docker-compose.yml 
+curl -o /opt/hysteria-server/cloudflare.ini https://o.oo0o.ooo/scripts/installment/tutu/cloudflare.ini 
+curl -o /opt/hysteria-server/server.yaml https://o.oo0o.ooo/scripts/installment/tutu/server.yaml 
+
+sed -i 's/restart: unless-stopped/restart: "no"/' /opt/hysteria-server/docker-compose.yml
+sed -i '/certonly/,/-d \*.tutusub.xyz/ s/^/#/' /opt/hysteria-server/docker-compose.yml
+sed -i '/^#/ a\    command: /bin/true' /opt/hysteria-server/docker-compose.yml
+sed -i '/certbot_etc/a\      - ./certs/live:/etc/letsencrypt/live' /opt/hysteria-server/docker-compose.yml
+
+docker compose -f /opt/hysteria-server/docker-compose.yml up -d hysteria
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y iptables iproute2 iptables-persistent
+
+iptables -t nat -A PREROUTING -p udp --dport 20000:30000 -j REDIRECT --to-port 9443
+
+netfilter-persistent save
+docker compose -f /opt/hysteria-server/docker-compose.yml restart hysteria
+
+echo "Hysteria服务已启动，使用导入的证书文件"
